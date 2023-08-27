7585fe3f585f20680dd2425cd8e013dc88e91847
FelixChent <citychen1062@gmail.com>
2023-08-27 10:34:37 +0800

    Initial Commit

---
+++ TutorialsOne/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TutorialsOne
+//
+//  Created by FelixChen on 2023/8/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TutorialsOne/TutorialsOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialsOneApp.swift
+//  TutorialsOne
+//
+//  Created by FelixChen on 2023/8/27.
+//
+
+import SwiftUI
+
+@main
+struct TutorialsOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
